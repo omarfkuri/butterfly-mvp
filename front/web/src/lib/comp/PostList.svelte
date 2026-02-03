4c01942cf1b18a4412f48f40d7b50389e17f4c26
Omar <omarfkuri@gmail.com>
2026-02-03 17:42:29 -0600

    Post List

---
+++ front/web/src/lib/comp/PostList.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+	import type { Post, User } from '$lib';
+
+	type Topic = (
+		|"all"
+		|`author:${string}`
+	);
+
+	interface Props
+	{
+		posts: Post[]
+		topic: Topic
+		user: User | null
+	}
+
+	let { posts, user, topic }: Props = $props();
+
+	$effect(() => {
+		const path = `/server/events/posts/${topic}`;
+
+		const source = new EventSource(path, {
+			withCredentials: true
+		});
+
+		source.addEventListener('created', async () => {
+			const res = await fetch('/server/posts/all');
+			posts = await res.json();
+		});
+
+		source.addEventListener('deleted', async () => {
+			const res = await fetch('/server/posts/all');
+			posts = await res.json();
+		});
+
+		source.addEventListener('updated', async () => {
+			const res = await fetch('/server/posts/all');
+			posts = await res.json();
+		});
+
+		return () => source.close();
+	});
+
+	async function deletePost(id: string)
+	{
+		const res = await fetch(`/server/posts/delete/${id}`, {
+			method: "DELETE",
+			credentials: "include"
+		});
+	}
+
+</script>
+
+{#each posts as { title, content, username, id, createdAt }}
+	{@const date = new Date(createdAt).toLocaleString(undefined, {
+		dateStyle: "long",
+	})}
+	{@const time = new Date(createdAt).toLocaleString(undefined, {
+		// timeZone: "America/Mexico_City",
+		timeStyle: "short",
+		hour12: false
+	})}
+	<article class="post">
+		<div class="main">
+			<div class="title">{title}</div>
+			<div class="content">{content}</div>
+		</div>
+
+		<div class="info">
+			{#if user}
+				<div class="username">{username}, {date} {time}</div>
+
+				{#if username == user.username}
+					<div class="buttons">
+						<button>Update</button>
+						<button onclick={()=>deletePost(id)}>Delete</button>
+					</div>
+				{/if}
+			{/if}
+		</div>
+	</article>
+{:else}
+	No posts yet
+{/each}
+
+<style lang="less">
+	.post
+	{
+		display: flex;
+		flex-direction: column;
+		gap: .25em;
+
+		padding-block: .5em;
+		border-bottom: 1px solid #AAA;
+	}
+
+	.main
+	{
+		display: flex;
+		flex-direction: column;
+		gap: .1em;
+
+		.title
+		{
+			font-weight: bold;
+			font-size: 1.1em;
+		}
+
+		.content
+		{
+			font: 1em monospace;
+		}
+	}
+
+	.info
+	{
+		display: flex;
+		justify-content: space-between;
+
+		.username
+		{
+			font-size: .8em;
+		}
+	}
+</style>
