01ac984dc8e7e45fb79266cdf8f6838f65cd9842
Omar <omarfkuri@gmail.com>
2026-02-03 21:11:08 -0600

    User image and cover display

--- front/web/src/routes/(main)/user/[username]/+page.svelte
+++ front/web/src/routes/(main)/user/[username]/+page.svelte
@@ -9,7 +9,15 @@
 </script>
 
 <div class="user-profile">
-	<h2>{username}</h2>
+	<div class="cover">
+		<img src="/image/cover.png" alt="Cover">
+	</div>
+	<div class="info">
+		<div class="profile-picture">
+			<img src="/image/user.png" alt="User">
+		</div>
+		<div class="title">{username}</div>
+	</div>
 </div>
 
 <PostList {posts} {user} topic="author:{username}"/>
@@ -20,6 +28,61 @@
 	.user-profile
 	{
 		.card();
+
+		position: relative;
+
+		background: linear-gradient(10deg, 
+			@bg1, 
+			transparent 50%
+		);
+
+		.cover
+		{
+			position: absolute;
+			inset: 0;
+			z-index: -1;
+
+			width: 100%;
+			height: 100%;
+
+			overflow: hidden;
+
+			img
+			{
+				width: 100%;
+				height: 100%;
+
+				object-fit: cover;
+			}
+		}
+
+		.info
+		{
+			margin-top: 3em;
+			width: min-content;
+
+			.profile-picture
+			{
+				width: 50px;
+				height: 50px;
+
+				overflow: hidden;
+				border-radius: 50%;
+
+				img
+				{
+					width: 100%;
+					height: 100%;
+
+					object-fit: cover;
+				}
+			}
+
+			.title
+			{
+				text-align: center;
+			}
+		}
 	}
 
 </style>
