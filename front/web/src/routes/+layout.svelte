a985ad3c11a84c8cca168cda32d8a61ade1ce4b6
Omar <omarfkuri@gmail.com>
2026-07-12 18:30:57 -0600

    Add progress bar when navigating

--- front/web/src/routes/+layout.svelte
+++ front/web/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { navigating } from '$app/state';
 	import favicon from '$lib/assets/image/brand/Icon.png';
 
 	import "$lib/styles/global.less";
 
 	let { children } = $props();
+
+
 </script>
 
 <svelte:head>
@@ -11,4 +14,19 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </svelte:head>
 
+{#if navigating.to}
+<progress></progress>
+{/if}
+
 {@render children()}
+
+<style lang="less">
+	@import (reference) "../lib/styles/vars.less";
+	
+	progress
+	{
+		width: 100%;
+		color: @primary;
+	}
+
+</style>
